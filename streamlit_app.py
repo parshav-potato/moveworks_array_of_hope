09c0031b1e50641e0329ccbc059950e1961900f7
parshav-potato <102019571+parshav-potato@users.noreply.github.com>
2023-09-30 20:01:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,43 @@
-import requests
-from bs4 import BeautifulSoup
 import streamlit as st
-from io import BytesIO
-import re
-from PyPDF2 import PdfWriter
-from reportlab.pdfgen import canvas
+import random
+import time
 
+st.title("Simple chat")
 
-def extract_text_from_website(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.title.string.strip()
-    text = soup.get_text(separator=' ')
-    text = "\n".join(line for line in text.splitlines() if line.strip())
-    return text, title
-def main():
-    st.title("Website Text Extractor")
-    url = st.text_input("Enter the URL of the website:")
-    if st.button("Extract Text"):
-        if url:
-            try:
-                extracted_text, webpage_title = extract_text_from_website(url)
-                st.success("Text extraction successful!")
-                st.text_area("Extracted Text:", value=extracted_text, height=400)
-                pdf_bytes = BytesIO()
-                pdf_writer = PdfWriter()
-                c = canvas.Canvas(pdf_bytes)
-                c.setFont("Helvetica", 12)
-                c.drawString(50, 800, extracted_text)
-                c.save()
-                pdf_bytes.seek(0)
-                file_name = re.sub(r'[\\/:"*?<>|]+', '_', webpage_title) + ".pdf"
-                st.download_button("Download", data=pdf_bytes, file_name=file_name)
-            except Exception as e:
-                st.error("An error occurred during text extraction.")
-                st.error(str(e))
-        else:
-            st.warning("Please enter a URL.")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
